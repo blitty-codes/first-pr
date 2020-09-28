230cfe0b17037eca5fc00a7c765bb1a236258756
rokkoo <alfonsolasaguirre@gmail.com>
2020-09-28 15:14:51 +0200

    remove reducer

--- hooks/useAppContext.ts
+++ hooks/useAppContext.ts
@@ -1,42 +1,11 @@
+import { useContext } from 'react';
 import { AppContext } from 'context/AppContext';
-import { useContext, useReducer } from 'react';
-
-interface State {
-   langs: Array<string>;
-}
-
-interface ReducerProps {
-   state: State;
-   action: any;
-}
-
-const languageReducer = (state: State, action: any) => {
-   switch (action.type) {
-      case 'ADD':
-         console.log(action);
-         const langs = state.langs.push(action.lang);
-         console.log(langs);
-
-         return { ...state, langs: ['test'] };
-
-      default:
-         return state;
-   }
-};
-
-const init = (): State => {
-   return {
-      langs: ['javascript'],
-   };
-};
 
 const useAppContext = () => {
-   const [stat, dispatch] = useReducer(languageReducer, { langs: [] }, init);
    const { state, setState } = useContext(AppContext);
 
    const addLang = (lang: string) => {
       if (!state.lang.includes(lang)) {
-         dispatch({ type: 'ADD', lang });
          setState({ ...state, lang: [...state.lang, lang] });
       }
    };
@@ -49,7 +18,7 @@ const useAppContext = () => {
       setState({ ...state, lang });
    };
 
-   return { state, addLang, stat, removeLang };
+   return { state, addLang, removeLang };
 };
 
 export default useAppContext;
