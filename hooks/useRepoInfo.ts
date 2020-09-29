6d8c2097d4c4327ee4e76696f1133227679287b3
blitty <blittynet@gmail.com>
2020-09-29 18:04:24 +0200

    uninstalled octokit/rest

--- hooks/useRepoInfo.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-// get repos by topics https://docs.github.com/en/free-pro-team@latest/rest/reference/search#search-topics
-// https://github.com/octokit/rest.js
-const { Octokit } = require("@octokit/rest");
-const octokit = new Octokit();
