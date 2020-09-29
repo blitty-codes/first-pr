39ae439a56bd461d179f4034079a4eab665997b1
blitty <blittynet@gmail.com>
2020-09-29 22:41:28 +0200

    Card updated

--- components/Repos/Repo/index.tsx
+++ components/Repos/Repo/index.tsx
@@ -1,41 +1,80 @@
+import StarIcon from 'icons/star';
+import CommentIcon from 'icons/comment';
+import HomeSiteIcon from 'icons/homeSite';
+
 interface RepoProps {
   title: string;
   createdBy: string;
-  shortDescription: string;
-  score: number;
-  // link: string;
+  description: string;
+  homepage: string;
+  stars: number;
+  comments: string;
+  avatar_owner: string;
+  has_wiki: boolean;
 }
-// the link is generated with the title and createdBy
-// https://github.com/${createdBy}/${title}
 
-const Repo = ({ title, createdBy, shortDescription, score }: RepoProps) => (
-  <div className="xl:w-1/4 lg:w-1/4 ms:w-full md:w-full p-5 mr-4 ml-4 mb-8 border-2 border-gray-600 border-opacity-25 rounded shadow-lg overflow-auto break-all">
-    <div className="h-full border p-2 border-gray-400 border-opacity-25 rounded">
-      <div className="mb-6">
-        <h1 className="text-4xl text-center text-gray-800 font-bold">
-          <a href={`https://github.com/${createdBy}/${title}`}>
-            {title.toUpperCase()}
-          </a>
-        </h1>
-        <hr />
+const Repo = ({
+  title,
+  createdBy,
+  description,
+  homepage,
+  stars,
+  comments,
+  avatar_owner,
+  has_wiki,
+}: RepoProps) => (
+  <div className="xl:w-1/3 lg:w-1/3 ms:w-full md:w-full p-5 mr-4 ml-4 mb-8 border-2 border-gray-600 border-opacity-25 rounded shadow-lg ">
+    <div className="w-auto">
+      <div className="relative top-info">
+        <div className="inline-block m-auto w-auto p">
+          <div className="border-2 border-gray-600 border-opacity-25 rounded">
+            <StarIcon width={30} height={30} />
+          </div>
+          <p className="text-sm text-2xl text-center">{stars}</p>
+        </div>
+        <div className="inline-block m-auto">
+          <div className="border-2 border-gray-600 border-opacity-25 rounded">
+            <CommentIcon width={30} height={30} />
+          </div>
+          <p className="text-sm text-2xl text-center">{comments}</p>
+        </div>
       </div>
-      <h3 className="font-bold">
-        Created
-      </h3>
-      <h3 className="ml-2">
-        <a href={`https://github.com/${createdBy}`}>{createdBy}</a>
-      </h3>
-      <h5 className="font-bold">
-        Descrition
-      </h5>
-      <h5 className="ml-2 pb-1">
-        {shortDescription}
-      </h5>
-      <div className="inline-block pb-2">
-        <p className="text-sm align-text-bottom">Score: {score}</p>
+      <div className="border p-2 border-gray-400 border-opacity-25 rounded">
+        <div className="mb-6">
+          <h1 className="text-4xl text-center text-gray-800 font-bold focus:text-blue-600">
+            {title}
+          </h1>
+          <hr />
+        </div>
+        <div className="grid grid-cols-2 grid-flow-col gap-2">
+          <div className="row-span-3 p-2">
+            <div className="overflow-auto break-all">
+              <h5 className="font-bold">Descrition</h5>
+              <h5 className="ml-2 pb-1">{description}</h5>
+            </div>
+            <h3 className="font-bold">Created</h3>
+            <h3 className="ml-2">
+              <a href={`https://github.com/${createdBy}`}>{createdBy}</a>
+            </h3>
+          </div>
+          <div className="row-span-2 col-span-2 overflow-auto break-word">
+            <div className="">
+              <img src={avatar_owner} />
+            </div>
+            <div className="mt-4">
+              <a href={homepage} target="_blanck" className="flex flex-row">
+                <HomeSiteIcon width={20} height={20} />{" "}
+                <p className="w-min-0 text-base align-top font-bold">
+                  homepage
+                </p>
+              </a>
+            </div>
+            <div>Wiki: {has_wiki ? "Yes" : "No"}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-)
+);
 
 export default Repo;
--- components/Repos/index.tsx
+++ components/Repos/index.tsx
@@ -1,15 +1,38 @@
+import useFetchRepos from 'hooks/useFetchRepos';
+
 import Repo from './Repo';
 
 const Repos = () => {// mb-4
+  const { repos } = useFetchRepos();
+  let i = 0;
+
+  console.log(repos);
+
   return (
     <section className="flex flex-wrap mt-8 self-center">
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasdfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasadfasfasdfsadfasfasdfasfasdfsdfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasfasfasadfsaddfsadfsadfsadfsfdasfddfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasdfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasdfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasdfsadfasdfsafdsadfsadfasfasdfasfdd" score={19}  />
-      <Repo title="first-pr" createdBy="Alfonso" shortDescription="Hello world fsadfasfdasdfsadfsadfsadfsadfsafdsadfasfasdfasfdd" score={19}  />
+      <Repo
+        title="Issue issue #444"
+        createdBy="World"
+        description="Hello world, dfafsdafasfdsadfsafdsfd"
+        homepage="https://IwI"
+        stars={15}
+        comments="5"
+        avatar_owner="https://AvatarWorld"
+        has_wiki="fsafasfdasf"
+      />
+      {repos.map((repoInfo) => (
+        <Repo
+          key={i++}
+          title={repoInfo.title}
+          createdBy="NOMBRE USUARIO CREADOR"
+          description="DESCRIPCION FALTA"
+          homepage={repoInfo.homepage}
+          stars={repoInfo.stargazers_count}
+          comments={repoInfo.comments}
+          avatar_owner={repoInfo.owner.avatar_url}
+          has_wiki={repoInfo.has_wiki}
+        />
+      ))}
     </section>
   );
 };
